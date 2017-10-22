e6273e9689cb026683a275f91ae3789287f3b559
Adhitya Ramadhanus <adhitya@payfazz.com>
2017-10-22 22:33:10 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # importir
 Bash script to list your actual dependencies of your node.js project, support CommonJS require and ES6 import
+
+<p>
+  <a href="#how-it-works">How it works |</a>
+  <a href="#installation">Installation |</a>
+  <a href="#in-action">In Action | </a>
+  <a href="#todo">Todo |</a>
+  <a href="#licenses">License</a>
+  <br><br>
+  <blockquote>
+  Bash script to list your actual dependencies of your node.js project
+  
+  support CommonJS require and ES6 import
+  </blockquote>
+</p>
+
+How It Works
+------------
+```
+Dependencies will be listed by applying regex to every js file in target directory
+
+There are 2 regex that will be used to determine a dependency
+require regex, require\(['\"]\K([a-zA-Z][^'\"/]*)(?=['\"]\))
+import regex, import\s(.*)\sfrom\s['\"]\K([a-zA-Z][^'\"/]*)(?=['\"])
+These two regex will be applied to js file using grep with Perl Regex options (-P)
+
+Notes: These are simple regex and may not be reliable in some case (only tested for few cases)
+```
+
+
+Installation
+----------- 
+* git clone
+* chmod +x importir.sh (optional)
+* move to your PATH, maybe remove .sh if you want (optional)
+```bash
+  NAME:
+    importir - bash script to list node.js project dependencies
+
+  USAGE:
+    importir command [command options] [arguments...]
+
+  VERSION:
+    0.0.1
+
+  AUTHOR:
+    Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
+
+  COMMANDS:
+      list        List all dependencies in target dir (current directory by default)
+      help        See help
+      version     See current version
+```
+In Action
+----------------
+(soon)
+
+Todo
+----------------
+* I don't know, give me some suggestions
+
+License
+----
+
+MIT © [Adhitya Ramadhanus]
+
---
+++ importir.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+VERSION=0.0.1
+REQUIRE_REGEX="require\(['\"]\K([a-zA-Z][^'\"/]*)(?=['\"]\))"
+IMPORT_REGEX="import\s(.*)\sfrom\s['\"]\K([a-zA-Z][^'\"/]*)(?=['\"])"
+
+function get_javascript_files {
+  local TARGET_DIR="${1:-.}"
+  # global var
+  files=($(find $TARGET_DIR -type f -name '*.js'))
+}
+
+function get_external_dep {
+  declare -A GLOBAL_DEPS
+  for file in "${files[@]}"; do
+    # require
+    local local_req_deps=($(grep -oP $REQUIRE_REGEX $file))
+    # improt
+    local local_import_deps=($(grep -oP $IMPORT_REGEX "$file"))
+    if [[ "${#local_req_deps[@]}" -gt 0 ]]; then
+      for dependency in "${local_req_deps[@]}"; do
+        GLOBAL_DEPS[${dependency}]=1
+      done
+    fi
+
+    if [[ "${#local_import_deps[@]}" -gt 0 ]]; then
+      for dependency in "${local_import_deps[@]}"; do
+        GLOBAL_DEPS[${dependency}]=1
+      done
+    fi
+
+  done
+
+  for dependency in "${!GLOBAL_DEPS[@]}"; do
+    echo "$dependency"
+  done
+}
+
+function main {
+  if [[ -z "$1" ]]; then 
+    print_usage
+    exit 1
+  else
+    local cmd="${1:-}"; shift
+    case "$cmd" in
+      list)
+        get_javascript_files "$1"
+        get_external_dep
+      ;;
+      version)
+        echo "$VERSION"
+      ;;
+      *)
+        print_usage
+      ;;
+    esac
+  fi
+}
+
+function print_usage {
+cat <<EOF
+  NAME:
+    importir - bash script to list node.js project dependencies
+
+  USAGE:
+    importir command [command options] [arguments...]
+
+  VERSION:
+    $VERSION
+
+  AUTHOR:
+    Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
+
+  COMMANDS:
+      list        List all dependencies in target dir (external by default)
+      help        See help
+      version     See current version
+EOF
+}
+
+main "$@"
